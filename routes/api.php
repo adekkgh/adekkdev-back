1ab66af3dbfdaa2a5787451084117e85aa1ede95
GioevArsen <gioev.arsen15@list.ru>
2025-02-27 01:34:07 +0300

    test/db-migrations

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', function () {
-    $names = DB::table('test')->pluck('name');
+    $names = DB::table('test')->select('name', 'isTest')->get();
 
     return response()->json($names);
 });
