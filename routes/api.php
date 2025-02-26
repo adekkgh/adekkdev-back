9dda7663bcbee4299f93a0b0deba3fdf638e04e0
GioevArsen <gioev.arsen15@list.ru>
2025-02-26 23:53:52 +0300

    test/autodeploy

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', function () {
-    return response()->json('sosal?');
+    return response()->json('sosal? with autodeploy');
 });
