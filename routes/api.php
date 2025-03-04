7707ea353bfc7199de623bc705b07ed605f48bdc
GioevArsen <gioev.arsen15@list.ru>
2025-03-04 13:05:04 +0300

    fix/test table migration and changed api/test endpoint response

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', function () {
-    $names = DB::table('test')->select('name', 'isTest')->get();
+    $names = DB::table('test')->select('name', 'rndNumber')->get();
 
     return response()->json($names);
 });
