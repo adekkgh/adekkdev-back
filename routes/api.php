42b87dc91181b9b7a44d40c09b6ddb224a01dd6e
GioevArsen <gioev.arsen15@list.ru>
2026-06-09 14:46:35 +0300

    testing deploy

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/test', function () {
-    $names = DB::table('test')->select('name', 'rndNumber')->get();
+    $test = '1234 hello world';
 
-    return response()->json($names);
+    return response()->json($test);
 });
